e28649543c26f0d311b07c8a324f30c5a8328cd8
jongbin26 <81405795+jongbin26@users.noreply.github.com>
2021-11-06 18:48:55 +0900

    nonogram

--- js/start.js
+++ js/start.js
@@ -1,8 +1,46 @@
 // start html
 for(i=0;i<localStorage.length;i++){
   temp="game"+(i+1);
-  console.log(temp);
+  table="<table><tr><td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr><td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr><td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr></table>";
   const litag=document.createElement('li');
-  litag.innerHTML=temp;
+  const fronttag=document.createElement('div');
+  const backtag=document.createElement('div');
+  const previous=document.createElement('div');
+  fronttag.className="front";
+  backtag.className="back";
+  previous.className="previous";
+  fronttag.innerHTML=temp;
+  backtag.innerHTML=table;
+  previous.innerHTML="<";
+  backtag.append(previous);
+  litag.append(fronttag);
+  litag.append(backtag);
   document.getElementsByClassName("gamelist")[0].append(litag);
 }
+
+const fronts=document.querySelectorAll('.front');
+fronts.forEach(function(front){
+  front.addEventListener('click',function(){
+    front.nextSibling.classList.add('on');
+  })
+});
+
+const previouses=document.querySelectorAll('.previous');
+previouses.forEach(function(previous){
+  previous.addEventListener('click',function(){
+    previous.parentElement.classList.remove('on');
+  })
+});
+
+//start html에서 td에 black class 추가
+// create html에서 td 버튼
+const tds=document.querySelectorAll('td');
+tds.forEach(function(td){
+  td.addEventListener('click',function(){
+    if(td.classList.contains("black")){
+      td.classList.remove('black');
+    }else{
+      td.classList.add('black');
+    }
+  })
+});  
