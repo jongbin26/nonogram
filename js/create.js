93a20bc361cec26546d063af754fd64eaded39c6
jongbin26 <81405795+jongbin26@users.noreply.github.com>
2021-11-07 16:25:50 +0900

    nonogram

--- js/create.js
+++ js/create.js
@@ -1,37 +1,66 @@
-// create html에서 td 버튼
-const tds=document.querySelectorAll('td');
-tds.forEach(function(td){
-  td.addEventListener('click',function(){
-    if(td.classList.contains("black")){
-      td.classList.remove('black');
-    }else{
-      td.classList.add('black');
-    }
-  })
-});  
-
-// create html에서 추가 버튼
-const plus=document.querySelector(".plus");
-plus.addEventListener('click',function(){
-  var temp=null;
-  tds.forEach(function(td){
-    if(td.classList.contains("black")){
-      temp+="1";
-    }
-    else{
-      temp+="0";
-    }
-  });
-  //local storage에 저장
-  var index=String(localStorage.length);
-  temp=temp.substring(4);
-  wrongpaint="0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-  if(temp!=wrongpaint)
-    localStorage.setItem(index,temp);
-  else
-    window.alert("비어있는 도안입니다.");
-  // 도안 지우기
-  tds.forEach(function(td){
-    td.classList.remove("black");
-  });
+create();
+// level btn
+board=document.querySelector('.board');
+level1=document.querySelector('.level1');
+level2=document.querySelector('.level2');
+level3=document.querySelector('.level3');
+console.log(board);
+level1.addEventListener('click',function(){
+  board.innerHTML="<table>  <tr>    <td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td>  </tr></table>";
+  create();
 });
+level2.addEventListener('click',function(){
+  board.innerHTML="<table>  <tr>    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>  </tr>  <tr>    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>  </tr></table>";
+  create();
+});
+level3.addEventListener('click',function(){
+  board.innerHTML="<table><tr><td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr><td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr><td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr><tr>  <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td></tr></table>";
+  create();
+});
+function create(){ 
+  // create html에서 td 버튼
+  const tds=document.querySelectorAll('td');
+  tds.forEach(function(td){
+    td.addEventListener('click',function(){
+      if(td.classList.contains("black")){
+        td.classList.remove('black');
+      }else{
+        td.classList.add('black');
+      }
+    })
+  });  
+
+  // create html에서 추가 버튼
+  const plus=document.querySelector(".plus");
+  plus.addEventListener('click',function(){
+    var temp=0;
+    console.log(temp);
+    tds.forEach(function(td){
+      if(td.classList.contains("black")){
+        temp+="1";
+      }
+      else{
+        temp+="0";
+      }
+    });
+    //local storage에 저장
+    if (isUnValidMap(temp.substring(1))) {
+      return ;
+    }
+    var index=String(localStorage.length);
+    temp=temp.substring(1);
+    console.log(temp);
+    localStorage.setItem(index,temp);
+    tds.forEach(function(td){
+      td.classList.remove("black");
+    });
+  });
+}
+
+function isUnValidMap (board) {
+  return board.split('')
+            .every(function (v)
+            {
+              return v === '0';
+            });
+}
